c663cf7abb94eb391d54f2da0fa1067c9219e56e
Paramesh Gunasekaran <paramesh.gunasekaran@gmail.com>
2021-09-05 09:15:51 +1000

    update: added links ui

---
+++ Krystal.Web/Models/Link.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Krystal.Web.Models
+{
+    public class Link
+    {
+        public Guid Id { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public string Slug { get; set; }
+
+        public string Url { get; set; }
+
+        public DateTime? Expiry { get; set; }
+    }
+}
